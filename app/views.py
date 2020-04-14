006118ff9f1e446f2671a3cd2083898be82c107f
lowsukuku_linux <lowsukuku_linux@lsk.com>
2020-04-14 18:26:29 +0300

    added new templates, added user and answers stubs

--- app/views.py
+++ app/views.py
@@ -3,25 +3,36 @@ from django.http import HttpRequest, Http404
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 # Create your views here.
 from random import randint
+from datetime import datetime
+from time import time
 
 tags = [
     f'tag {i}'
     for i in range(100)
 ]
 
+user = {
+    'name': 'nibba'
+}
+
+answers = {
+    i: {'id': i, 'text': f'Hello world! This is my answer {i}',
+        'rating': randint(-100, 100), 'creator': user, 'creationTime': datetime.now()}
+    for i in range(100)
+}
+
 questions = {
     i: {'id': i, 'title': f'question {i}',
         'text': f'Hello world! This is my question {i}', 'rating': randint(-100, 100), 'tags': [
             tags[randint(0, 99)]
             for j in range(randint(1, 5))
-        ]}
+        ], 'creator': user, 'creationTime': datetime.now(), 'answers': answers, 'answersCount': len(answers)}
     for i in range(100)
 }
 
 
 def newQuestions(request):
     page, pages = paginate(list(questions.values()), request, 10)
-
     return render(request, 'index.html', {
         'questions': page,
         'pages': pages
